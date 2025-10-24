0d9a675bfad05365fd46cd1edaecea2f38abf8b2
Nighty <flander1059@gmail.com>
2025-10-24 21:24:22 +0300

    implemetation reviews section

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,22 +1,14 @@
 function initSlider() {
-  console.log(123)
-
-  const reviewsSlider = new Swiper('.reviews__block', {
-    loop: true,
-    slidesPerView: 1,
-    spaceBetween: 150,
-
-    // Пагинация
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-
-    // Навигационные стрелки
+  let sliderReviews = new Swiper(".reviews__body", {
+    cssMode: true,
     navigation: {
-      nextEl: '.reviews__slider-button-next',
-      prevEl: '.reviews__slider-button-prev',
+      nextEl: ".reviews__slider-button-next",
+      prevEl: ".reviews__slider-button-prev",
     },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
   });
 };
 
