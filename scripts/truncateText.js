f79417e9e933694c364351f9018867f2532e69e0
Nighty <flander1059@gmail.com>
2025-10-28 11:11:33 +0300

    Add truncate function js

--- scripts/truncateText.js
+++ scripts/truncateText.js
@@ -1,6 +1,15 @@
-const descriptionsCards = document.querySelectorAll('[data-js-article-description]')
+const descriptionsCards = document.querySelectorAll('[data-js-article-description]');
+const acceptableCountWords = 17;
 
 function truncateText() {
+  descriptionsCards.forEach(description => {
+    let wordsDescriptionArr = description.textContent.trim().split(/\s+/).filter(word => word.length > 0);
+
+    if (wordsDescriptionArr.length > acceptableCountWords) {
+      wordsDescriptionArr.splice(acceptableCountWords);
+      description.textContent = wordsDescriptionArr.join(' ') + '...';
+    }
+  });
 }
 
-export default truncateText
+export default truncateText;
