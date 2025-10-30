f13c64a4c3c26fc396a827bf378ceedad0c821ad
Nighty <flander1059@gmail.com>
2025-10-30 15:19:31 +0300

    footer implemetation and small fixes

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,6 +1,7 @@
 function initSlider() {
   let sliderReviews = new Swiper(".reviews__body", {
     cssMode: true,
+    spaceBetween: 30,
     navigation: {
       nextEl: ".reviews__slider-button-next",
       prevEl: ".reviews__slider-button-prev",
